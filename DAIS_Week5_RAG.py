a280c603147c8825141956946394baf3f09c6dee
iayushi <iayushi@users.noreply.github.com>
2025-10-19 13:17:58 +0100

    Refactor imports and clean up unused code
    Updated imports and commented out unused code for clarity.

--- DAIS_Week5_RAG.py
+++ DAIS_Week5_RAG.py
@@ -6,22 +6,28 @@ import sys
 import pysqlite3
 sys.modules["sqlite3"] = pysqlite3
 from langchain_community.vectorstores import Chroma
-from langchain.storage import InMemoryStore
-from langchain.schema import Document
-from langchain.retrievers.multi_vector import MultiVectorRetriever
+# from langchain.storage import InMemoryStore
+from langchain_core.stores import InMemoryStore
+# from langchain.schema import Document
+from langchain_core.documents import Document
+# from langchain.retrievers.multi_vector import MultiVectorRetriever
+from langchain_classic.retrievers.multi_vector import MultiVectorRetriever
+# from langchain_community.retrievers.multi_vector import MultiVectorRetriever
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_together import ChatTogether
+# from langchain_together import ChatTogether
 from langchain_groq import ChatGroq
 from langchain_community.chat_models import ChatHuggingFace
 #from langchain_community.chat_models import ChatPerplexity
 from langchain_anthropic import ChatAnthropic
 from langchain_perplexity import ChatPerplexity
-from langchain.prompts import ChatPromptTemplate
+#from langchain.prompts import ChatPromptTemplate
+from langchain_classic.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_huggingface import HuggingFaceEmbeddings
 from sentence_transformers import SentenceTransformer
-from langchain.schema import Document
+# from langchain.schema import Document
+from langchain_classic.schema import Document
 import traceback
 from operator import itemgetter
 import torch
@@ -36,7 +42,8 @@ st.subheader ("Week 5:  New Business Models as digital AI Strategy & Finding Pla
 # Sidebar: Choose provider & keys
 provider = st.sidebar.selectbox(
     "Choose LLM Provider:",
-    ("OpenAI", "Together", "Groq", "Hugging Face", "Anthropic", "Perplexity")
+    ("OpenAI", # "Together", 
+     "Groq", "Hugging Face", "Anthropic", "Perplexity")
 )
 api_key = st.sidebar.text_input(f"{provider} API Key", type="password")
 model_name = st.sidebar.text_input("Model name (optional)", "")
@@ -135,12 +142,12 @@ if api_key:
                 temperature=0.7
             )
 
-        elif provider == "Together":
-            model = ChatTogether(
-                together_api_key=api_key,
-                model=model_name or "mistralai/Mistral-7B-Instruct-v0.2",
-                temperature=0.7
-            )
+       # elif provider == "Together":
+       #     model = ChatTogether(
+       #         together_api_key=api_key,
+       #         model=model_name or "mistralai/Mistral-7B-Instruct-v0.2",
+       #         temperature=0.7
+       #     )
 
         elif provider == "Groq":
             model = ChatGroq(
